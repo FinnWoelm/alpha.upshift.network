f1e6ee7c0a5be16849a91818d0bc1f198a4ddb74
Finn Woelm <public@finnwoelm.com>
2016-07-03 00:14:38 +0200

    Add test: Profile page requires log-in
    Viewing a user's profile page should require log-in.

--- spec/features/profile_spec.rb
+++ spec/features/profile_spec.rb
@@ -2,6 +2,12 @@ require 'rails_helper.rb'
 
 feature 'Profile' do
 
+  scenario 'User is not logged in' do
+    given_i_am_not_logged_in
+    when_i_visit_the_page_of_another_user
+    then_i_should_see_the_login_page
+  end
+
   scenario 'User visits own profile' do
     given_i_am_logged_in_as_a_user
     when_i_visit_my_own_page
@@ -12,6 +18,11 @@ feature 'Profile' do
     given_i_am_logged_in_as_a_user
     when_i_visit_the_page_of_another_user
     then_i_should_see_their_profile
+  end
+
+  def given_i_am_not_logged_in
+    @user = create(:user)
+    visit logout_path
   end
 
   def given_i_am_logged_in_as_a_user
@@ -26,13 +37,18 @@ feature 'Profile' do
     visit user_path @user.username
   end
 
-  def then_i_should_see_my_profile
-    expect(page).to have_content(@user.name)
-  end
-
   def when_i_visit_the_page_of_another_user
     @another_user = create(:user)
     visit user_path @another_user.username
+  end
+
+  def then_i_should_see_the_login_page
+    expect(current_path).to eq(login_path)
+    expect(page).to have_content("Login")
+  end
+
+  def then_i_should_see_my_profile
+    expect(page).to have_content(@user.name)
   end
 
   def then_i_should_see_their_profile
