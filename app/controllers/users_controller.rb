2af124b64d8c9e44268b55f15c635edbabcdf3be
Finn Woelm <public@finnwoelm.com>
2016-07-02 21:56:40 +0200

    Add session logic and login form
    User can now log in (you need to create a user manually first). This allows
    user to access views of the User controller/model.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_filter :authorize
 
   # GET /users
   # GET /users.json
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+
+  # SessionsController
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'users#index'
 end
