090aacabd321cfed14cc7d6e2abdfa8897771c61
Finn Woelm <public@finnwoelm.com>
2016-07-02 23:01:29 +0200

    Enforce username format a-z, numbers, underscores

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :user do
     email { Faker::Internet.email }
-    username { Faker::Internet.user_name }
+    username { Faker::Internet.user_name( Faker::Name.name, %w(_) ) }
     password { Faker::Internet.password(10, 50) }
     name { Faker::Name.name }
     last_seen_at nil
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,9 +8,8 @@ RSpec.describe User, type: :model do
     build(:user).should be_valid
   end
 
-  # For future
-  # it "is invalid without an email" do
-  #   build(:user, email: nil).should_not be_valid
-  # end
+  it "is invalid with illegal username" do
+    build(:user, :username => "some.username").should_not be_valid
+  end
 
 end
