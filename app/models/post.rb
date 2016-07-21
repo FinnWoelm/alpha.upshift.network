bee0c22a885afb76d43c8691208619c2a26bf697
Finn Woelm <public@finnwoelm.com>
2016-07-21 22:11:20 +0200

    Fetch posts in order of most recent to oldest
    This determines the order in which posts are displayed on user profiles (most
    recent to oldest).

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :author, :class_name => "User"
 
+  default_scope -> { order('created_at DESC') }
+
   validates :author, presence: true
   validates :content, presence: true
   validates :content, length: { maximum: 5000 }
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -10,4 +10,15 @@ RSpec.describe Post, type: :model do
     expect(build(:user)).to be_valid
   end
 
+  it "retrieves posts in order of most recent to oldest" do
+    @user = create(:user)
+    5.times { |i| create(:post, :author => @user, :created_at => Time.now - i * 1.day ) }
+
+    time_of_previous_post = Time.now + 100.days
+    @user.posts.each do |post|
+      expect(post.created_at).to be < time_of_previous_post
+      time_of_previous_post = post.created_at
+    end
+  end
+
 end
