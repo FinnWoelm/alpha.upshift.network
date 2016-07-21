e420df1419969d2d007efec219bf11348ff11b05
Finn Woelm <public@finnwoelm.com>
2016-07-22 00:29:29 +0200

    Feature spec for commenting function

--- spec/views/posts/show.html.erb_spec.rb
+++ spec/views/posts/show.html.erb_spec.rb
@@ -11,4 +11,17 @@ RSpec.describe "posts/show", type: :view do
     expect(rendered).to match(/Some text/)
     expect(rendered).to match(render_timestamp(@post.created_at))
   end
+
+  it "shows new comment form if user is signed in" do
+    @current_user = create(:user)
+    render
+    expect(rendered).to match("New Comment")
+  end
+
+  it "does not show new comment form if user is not signed in" do
+    @current_user = nil
+    render
+    expect(rendered).not_to match("New Comment")
+  end
+
 end
