151c4ba0ee2033e539c43126fd7800b8d8382c15
Finn Woelm <public@finnwoelm.com>
2016-07-21 21:04:44 +0200

    Limit post size to 5000 chars

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :author, :class_name => "User"
+
+  validates :author, presence: true
+  validates :content, presence: true
+  validates :content, length: { maximum: 5000 }
+
 end
