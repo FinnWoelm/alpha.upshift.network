f962f8730b6dd7db33eb36c854cc1dbb977af61b
Finn Woelm <public@finnwoelm.com>
2016-07-23 12:28:44 +0200

    Randomize order of rspec tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -51,6 +51,9 @@ RSpec.configure do |config|
   # instead of true.
   config.use_transactional_fixtures = false
 
+  # Randomize order in which tests are run
+  config.order = "random"
+
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
