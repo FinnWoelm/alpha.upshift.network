72fcb087ce84b69826e53cff62c6a03a8c7bdd36
Finn Woelm <public@finnwoelm.com>
2016-08-15 10:25:48 +0200

    Rewrite specs for and refactor friendship_request model

--- app/models/friendship_request.rb
+++ app/models/friendship_request.rb
@@ -3,11 +3,10 @@ class FriendshipRequest < ApplicationRecord
   belongs_to :recipient, :class_name => "User"
 
   # finds the record that contains the friendship request between two users
-  scope :find_friendship_request_between,
+  scope :find_friendship_requests_between,
     ->(user_one, user_two) {
       where(:sender => user_one).where(:recipient => user_two).
-        or(FriendshipRequest.where(:sender => user_two).where(:recipient => user_one)).
-        first
+        or(where(:sender => user_two).where(:recipient => user_one))
     }
 
   validates :sender, presence: true
@@ -18,25 +17,24 @@ class FriendshipRequest < ApplicationRecord
     :friendship_must_not_already_exist,
     if: "sender.present? and recipient.present?"
 
-  def recipient_profile_must_be_viewable_by_sender
-    if not recipient.profile.viewable_by?(sender)
-      errors[:base] << "User does not exist or profile is private"
+  private
+    def recipient_profile_must_be_viewable_by_sender
+      unless recipient.profile.viewable_by?(sender)
+        errors[:base] << "User does not exist or profile is private"
+      end
     end
-  end
 
-  def friendship_request_is_unique
-    if FriendshipRequest.where(sender: sender).where(recipient: recipient).any?
-      errors[:base] << "You have already sent a friend request to this user"
+    def friendship_request_is_unique
+      if FriendshipRequest.find_friendship_requests_between(sender, recipient).any?
+        errors[:base] << "A friendship request between #{sender.name} and " +
+                          "#{recipient.name} already exists."
+      end
     end
-    if FriendshipRequest.where(sender: recipient).where(recipient: sender).any?
-      errors[:base] << "#{recipient.name} has already sent a friend request to you"
-    end
-  end
 
-  def friendship_must_not_already_exist
-    if sender.has_friendship_with?(recipient)
-      errors[:base] << "You are already friends with #{recipient.name}"
+    def friendship_must_not_already_exist
+      if sender.has_friendship_with?(recipient)
+        errors[:base] << "You are already friends with #{recipient.name}"
+      end
     end
-  end
 
 end
