5ce62a3f4c0f25c25eaa68e41b83b24d5edf1d2f
Finn Woelm <public@finnwoelm.com>
2016-09-09 20:47:03 -0600

    Add seed data to test feed

--- db/seeds.rb
+++ db/seeds.rb
@@ -35,19 +35,32 @@ dennis = User.create(
 :password => "dennis"
 )
 
+[alice, bob, carla, dennis].each do |user|
+  Profile.create(:user => user)
+end
+
+# Create Friendships between users
+Friendship.create(:initiator => alice, :acceptor => bob)
+Friendship.create(:initiator => alice, :acceptor => carla)
+Friendship.create(:initiator => alice, :acceptor => dennis)
 
 # Create some private conversations
 conversations = []
 20.times do |i|
  u = User.create(:username => "user#{i}", :password => "user#{i}", :name => "user#{i}", :email => "user#{i}")
-  conversations << PrivateConversation.create(:sender => alice, :recipient => u)
+ Profile.create(:user => u)
 end
 
 # Create some private messages
 100.times do |i|
-  conversation = conversations[rand(0..conversations.size-1)]
+  conversation = PrivateConversation.new(:sender => alice, :recipient => User.order("RANDOM()").first)
   PrivateMessage.create(:sender => alice, :conversation => conversation, :content => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus quis placerat diam. Donec tempor consectetur sem ac accumsan. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Proin aliquam justo vel massa vehicula, id finibus magna auctor. Vivamus luctus tristique quam, in molestie orci convallis sed.")
   if (i+1)%10 == 0
     puts "Generated #{i+1} messages"
   end
+end
+
+# Create some posts
+100.times do |i|
+  Post.create(:author => User.order("RANDOM()").first, :content => "Post #{i}\nLorem Ipsum Dolorem")
 end
