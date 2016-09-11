c95786f3ed1539a22bcf9f435afcef3ef4c070ca
Finn Woelm <public@finnwoelm.com>
2016-09-10 20:02:28 -0600

    Use JS in newsletter subscription feature spec
    Use JS webkit driver for the Newsletter Subscription feature test.

--- spec/features/newsletter_subscription_spec.rb
+++ spec/features/newsletter_subscription_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper.rb'
 
 feature 'Newsletter Subscription' do
 
-  scenario 'Visitor can subscribe' do
+  scenario 'Visitor can subscribe', :js => true do
     given_i_visit_the_home_page
     when_i_click_join
     and_i_submit_my_information
@@ -14,18 +14,19 @@ feature 'Newsletter Subscription' do
   end
 
   def when_i_click_join
-
     click_on "Join"
   end
 
   def and_i_submit_my_information
-    fill_in 'name',   with: 'my name'
-    fill_in 'email',  with: 'user@example.com'
-    click_on 'Join'
+    @name = Faker::Name.name
+    @email = Faker::Internet.email
+    fill_in 'name',   with: @name
+    fill_in 'email',  with: @email
+    click_on 'Join Our Newsletter'
   end
 
   def then_i_should_see_a_success_message
-    expect(page).to have_content "Thank you for joining"
+    expect(page).to have_content "Thank you for joining, #{@name}!"
   end
 
 end
