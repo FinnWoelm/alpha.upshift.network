54e9b72397322713d9f891bd26fa614cdc656838
Finn Woelm <public@finnwoelm.com>
2017-02-20 23:28:09 +0600

    Show custom error page for profile not found or not visible
    When a user visits a URL that responds to ProfilesController#show, but that
    username does not exist or is not visible to the user, then we display a
    custom error page with detailed information on what happened.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,8 +5,7 @@ class ProfilesController < ApplicationController
 
   # GET /:username
   def show
-    render_404 and return unless @profile
-    render_404 and return unless @profile.viewable_by?(@current_user)
+    render('error', status: 404, layout: 'errors') and return unless @profile and @profile.viewable_by?(@current_user)
 
     @posts = @user.posts.most_recent_first.with_associations
     @post = Post.new
