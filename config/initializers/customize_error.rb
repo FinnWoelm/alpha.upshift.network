f6a1b02550e3c0fc951e25d2dd4f11c12fcebff4
Finn Woelm <public@finnwoelm.com>
2017-02-21 21:48:09 +0600

    Show validation errors underneath respective input field
    If an error occurs during validation, show the error right next to the
    corresponding input field.

--- config/initializers/customize_error.rb
+++ config/initializers/customize_error.rb
@@ -3,11 +3,33 @@ ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
 
   html = %(#{html_tag}).html_safe
 
-  elements = Nokogiri::HTML::DocumentFragment.parse(html_tag).css "label, input, textarea"
+  form_fields = [
+    'textarea',
+    'input',
+    'select'
+  ]
+
+  elements = Nokogiri::HTML::DocumentFragment.parse(html_tag).css "label, " + form_fields.join(', ')
   elements.each do |e|
-    e['class'] ||= ""
-    e['class'] += " invalid"
-    html = %(#{e}).html_safe
+
+    # add list of errors
+    if e.node_name.eql? 'label'
+      html += "<ul class='errors browser-default'>".html_safe
+
+      instance.error_message.each do |error|
+        html += "<li>#{error}</li>".html_safe
+      end
+
+      html += "</ul>".html_safe
+
+    # add class 'invalid'
+    elsif form_fields.include? e.node_name
+      e['class'] ||= ""
+      e['class'] += " invalid"
+
+      html = %(#{e}).html_safe
+    end
+
   end
 
   # return output
