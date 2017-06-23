b69a1948d57bbd8b4f524707cda70e319d8ebe8f
Finn Woelm <public@finnwoelm.com>
2017-06-23 12:29:12 +0700

    Fix: "Couldn't digest template" error when serving images

--- app/controllers/user/attachments_controller.rb
+++ app/controllers/user/attachments_controller.rb
@@ -8,7 +8,6 @@ class User::AttachmentsController < ApplicationController
 
     if user and user.send(attachment.to_sym).present? and user.viewable_by?(@current_user)
       expires_in 365.days
-      fresh_when user.send(attachment.to_sym).updated_at, public: false
       send_file(
         user.send(attachment.to_sym).path(size),
         filename: "#{user.username}_#{attachment}_#{size}.jpg",
